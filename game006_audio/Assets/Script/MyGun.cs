f754dd5db297a3cc24ebb03a2fa034353bbe1212
Wayne-MBP <506110230@qq.com>
2022-01-19 18:40:08 +0800

    add audio

---
+++ game006_audio/Assets/Script/MyGun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            float distance = (mousePos - transform.position).magnitude;
+            if (distance < 2)
+            {                
+                AudioSource audio = GetComponent<AudioSource>();
+                audio.PlayOneShot(audio.clip);
+                // audio.Play();
+            }
+        }
+    }
+}
---
+++ game006_audio/Assets/Script/MyPiano.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPiano : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown( 0 ))
+        {
+            // 判断：鼠标是否点中当前物体
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            float distance = (mousePos - transform.position).magnitude;
+            if(distance < 2 )
+            {
+                // 取得 AudioSource 组件
+
+                AudioSource audio = GetComponent<AudioSource>();
+                audio.Play();
+
+                //if(! audio.isPlaying)
+                //{
+                //    audio.Play();
+                //}
+
+                //if(audio.isPlaying)
+                //{
+                //    audio.Stop();
+                //}
+                //else
+                //{
+                //    audio.Play();
+                //}
+            }
+        }
+    }
+}
