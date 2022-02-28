a2127cc14a35f1630440812b11baeb5cbe3bdd67
Wayne-MBP <506110230@qq.com>
2022-02-28 22:30:55 +0800

    delete puzzle and add match 3

---
+++ Match 3/Assets/Scripts/Dot.cs
@@ -0,0 +1,184 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour
+{
+    [Header("Board Variables")]
+    public float swipeAngle = 0;
+    // 当前位置
+    public int column;
+    public int row;
+    // 上一次位置
+    public int previousColumn;
+    public int previousRow;
+    public int targetX;
+    public int targetY;
+    public bool isMatched = false;
+
+    /// <summary>
+    /// 私有变量
+    /// </summary>
+    private Board board;
+    private GameObject otherDot;
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    private Vector2 tempPosition;
+
+    private void Start()
+    {
+        board = FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        row = targetY;
+        column = targetX;
+        previousRow = row;
+        previousColumn = column;
+    }
+
+    private void Update()
+    {
+        FindMatches();
+        if (isMatched)
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.color = new Color(1, 1, 1, .2f);
+        }
+        targetX = column;
+        targetY = row;
+
+        if (Mathf.Abs(targetX - transform.position.x) > .1)
+        {
+            // 朝目标移动
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            // 直接设置
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+            board.allDots[column, row] = this.gameObject;
+        }
+
+        if (Mathf.Abs(targetY - transform.position.y) > .1)
+        {
+            // 朝目标移动
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            // 直接设置
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+            board.allDots[column, row] = this.gameObject;
+        }
+
+    }
+    /// <summary>
+    /// 交换后不匹配，等待0.5秒后还原
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator CheckMoveCo()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (otherDot != null)
+        {
+            if (!isMatched && !otherDot.GetComponent<Dot>().isMatched)
+            {
+                otherDot.GetComponent<Dot>().row = row;
+                otherDot.GetComponent<Dot>().column = column;
+
+                row = previousRow;
+                column = previousColumn;
+            }
+            otherDot = null;
+        }
+    }
+
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(firstTouchPosition);
+    }
+
+    private void OnMouseUp()
+    {
+        finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    private void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+        Debug.Log(swipeAngle);
+        MovePieces(); 
+    }
+    /// <summary>
+    /// 交换移动
+    /// </summary>
+    private void MovePieces()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width - 1)
+        {
+            //右交换
+            otherDot = board.allDots[column + 1, row];
+            otherDot.GetComponent<Dot>().column -= 1;
+            column += 1;
+        } else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height -1)
+        {
+            // 上交换
+            otherDot = board.allDots[column, row + 1];
+            otherDot.GetComponent<Dot>().row -= 1;
+            row += 1;
+        } else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0)
+        {
+            // 左交换
+            otherDot = board.allDots[column - 1, row];
+            otherDot.GetComponent<Dot>().column += 1;
+            column -= 1;
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135 && row > 0)
+        {
+            // 下交换
+            otherDot = board.allDots[column, row - 1];
+            otherDot.GetComponent<Dot>().row += 1;
+            row -= 1;
+        }
+        //协同程序
+        StartCoroutine(CheckMoveCo());
+    }
+    /// <summary>
+    /// 查找匹配并设置
+    /// </summary>
+    private void FindMatches()
+    {
+        if (column > 0 && column < board.width - 1)
+        {
+            GameObject leftDot1 = board.allDots[column - 1, row];
+            GameObject rightDot1 = board.allDots[column + 1, row];
+
+            if (leftDot1.tag == this.gameObject.tag && rightDot1.tag == this.gameObject.tag)
+            {
+                leftDot1.GetComponent<Dot>().isMatched = true;
+                rightDot1.GetComponent<Dot>().isMatched = true;
+                isMatched = true;
+            }
+        }
+
+        if (row > 0 && row < board.height - 1)
+        {
+            GameObject upDot1 = board.allDots[column, row + 1];
+            GameObject downDot1 = board.allDots[column, row - 1];
+
+            if (upDot1.tag == this.gameObject.tag && downDot1.tag == this.gameObject.tag)
+            {
+                upDot1.GetComponent<Dot>().isMatched = true;
+                downDot1.GetComponent<Dot>().isMatched = true;
+                isMatched = true;
+            }
+        }
+    }
+}
