6152af57fc7e1eaa704c9cb34bf07ccf0736bfd7
Wayne-MBP <506110230@qq.com>
2022-03-01 18:53:03 +0800

    重新填充

--- Match 3/Assets/Scripts/Board.cs
+++ Match 3/Assets/Scripts/Board.cs
@@ -112,5 +112,43 @@ public class Board : MonoBehaviour {
             nullCount = 0;
         } 
         yield return new WaitForSeconds(.4f);
+        StartCoroutine(FillBoardCo());
+    }
+    /// <summary>
+    /// 填充
+    /// </summary>
+    private void RefillBoard() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (allDots[i, j] == null) {
+                    Vector2 tempPosition = new Vector2(i, j);
+                    int dotToUse = Random.Range(0, dots.Length);
+                    GameObject piece = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                    allDots[i, j] = piece;
+                } 
+            }
+        }
+    }
+
+    private bool MatchesOnBoard() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (allDots[i, j] != null) {
+                    if (allDots[i, j].GetComponent<Dot>().isMatched) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator FillBoardCo() {
+        RefillBoard();
+        yield return new WaitForSeconds(0.5f);
+        while (MatchesOnBoard()) {
+            yield return new WaitForSeconds(0.5f);
+            DestroyMatches();
+        }
     }
 }
